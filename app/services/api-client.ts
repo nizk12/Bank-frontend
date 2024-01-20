a616aac3524dc495128ad513b634a84b3ca1331d
nizk12 <nirmalkhawas02@gmail.com>
2024-01-20 09:32:57 +0000

    update base url

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -1,4 +1,4 @@
 import axios, { CanceledError } from "axios";
 
-const apiClient = axios.create({ baseURL: 'https://abcbank-31lv.onrender.com/api' })
+const apiClient = axios.create({ baseURL: 'https://abcbank-xxh8.onrender.com/api' })
 export { apiClient, CanceledError } 
